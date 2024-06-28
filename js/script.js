f22139bd40036b1ed186d3176c4ef3ca097e0674
Jens Natzschka <jens.natzschka@posteo.de>
2024-06-28 22:15:27 +0200

    added like functionality

--- js/script.js
+++ js/script.js
@@ -1,25 +1,27 @@
 function renderBooks(){
     for (let i = 0; i < books.length; i++) {
         let book = books[i];
-        let bookCard = genreateCardHTML(book);
+        let bookCard = genreateCardHTML(i, book);
         
         document.getElementById('cards-container').innerHTML += bookCard;
+        generateLikeIcon(i);
+        generateLikeCounter(i);
     }
 }
 
-function genreateCardHTML(book){
-    let commentsSectionHTML = genreateCommentsHTML(book.comments);
-    let likeSectionHTML = genreateLikeHTML(book.likes, book.liked);
+function genreateCardHTML(i){
+    let commentsSectionHTML = genreateCommentsHTML(books[i].comments);
+    let likeSectionHTML = genreateLikeHTML(i);
     return `
         <div class="book-card">
         <div class="card-maininfo">
             <img class="book-image" src="img/book.webp" alt="">
             <div class="info-details">
-                <h2 class="book-title">${book.name}</h2>
-                <span class="book-autor">${book.author}</span>
-                <span class="book-genre">Genre: ${book.genre}</span>
-                <span class="book-publishedYear">${book.publishedYear}</span>
-                <span class="book-price">${book.price.toLocaleString('de-DE', { style: 'currency', currency: 'EUR' })}</span>
+                <h2 class="book-title">${books[i].name}</h2>
+                <span class="book-autor">${books[i].author}</span>
+                <span class="book-genre">Genre: ${books[i].genre}</span>
+                <span class="book-publishedYear">${books[i].publishedYear}</span>
+                <span class="book-price">${books[i].price.toLocaleString('de-DE', { style: 'currency', currency: 'EUR' })}</span>
             </div>
         </div>
         ${likeSectionHTML}
@@ -31,18 +33,18 @@ function genreateCardHTML(book){
     `;
 }
 
-function genreateLikeHTML(likes, likeStatus){
+function genreateLikeHTML(i){
     let likeIcon;
-    if(likeStatus){
+    if(books[i].liked){
        likeIcon = 'liked.png'
     } else {
         likeIcon = 'unliked.png'
     }
-    
+
     return `
         <div class="book-likes">
-            <span class="likes">${likes}</span>
-            <img class="like-icon" src="img/${likeIcon}" alt="">
+            <span id="likecounter-${i}" class="likes"></span>
+            <img id="likeicon-${i}" class="like-icon" onclick="toggleLike('${i}')">
         </div>
     `;
 }
@@ -63,6 +65,34 @@ function genreateCommentsHTML(comments){
     };
     return commentsHTML;
 }
+
+function toggleLike(i){
+    if(books[i].liked){
+        books[i].likes--;
+     } else {
+        books[i].likes++;
+     }
+
+     books[i].liked = !books[i].liked;
+
+     generateLikeIcon(i);
+     generateLikeCounter(i);
+}
+
+function generateLikeIcon(i){
+    let likeIcon;
+    if(books[i].liked){
+        likeIcon = 'liked.png'
+     } else {
+         likeIcon = 'unliked.png'
+     }
+     document.getElementById(`likeicon-${i}`).src = `img/${likeIcon}`
+}
+
+function generateLikeCounter(i){
+    document.getElementById(`likecounter-${i}`).innerHTML = books[i].likes;
+}
+
 
 function setup(){
     renderBooks();
